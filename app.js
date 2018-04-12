7d05381b46004f7ef693d104617ffea7b154287b
William Wise <WWise@corp.northamptonshire.gov.local>
2018-04-12 15:47:03 +0100

    adding in routes

---
+++ app.js
@@ -0,0 +1,38 @@
+var restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+// server.get('/hello/:name', respond);
+// server.head('/hello/:name', respond);
+//this will create a new skill in the skillz table
+server.post('/skill', function(req, res, next){
+	
+});
+//this will get skillz by ID
+server.get('/skill/:id', function(req, res, next){
+	
+});
+//this will create a uzer
+server.post('/uzer', function(req, res, next){
+	
+});
+//This will get uzer by name
+server.get('/uzer/:name', function(req, res, next){
+	
+});
+//get all uzerz
+server.get('/alluzerz', function(req, res, next){
+	
+});
+//this will give back a list of all the skillz in the db
+server.get('/allskillz', function(req, res, next){
+	
+});
+
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
