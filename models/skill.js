3fc2658bb482f28532ca74d05970f308485b9b09
SturatParker <stuart.parker@cambridgeshire.gov.uk>
2019-09-19 10:37:03 +0100

    Mongoosastic plugin
    Add mongoosastic plugin to skills schema

--- models/skill.js
+++ models/skill.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
+var mongoosastic = require('mongoosastic')
 var Schema = mongoose.Schema;
+
 // var passportLocalMongoose = require('passport-local-mongoose');
 // //var findOrCreate = require('mongoose-findorcreate');
 // //var serializer = require('passport-mongoose-serializer');
 
-var Skill = new Schema({
-	name: String
+var SkillSchema = new Schema({
+	name: { type: String, es_indexed: true }
 });
+
+SkillSchema.plugin(mongoosastic)
 
 // User.plugin(passportLocalMongoose);
 // //User.plugin(findOrCreate);
 
-module.exports = mongoose.model('Skill', Skill);
+module.exports = mongoose.model('Skill', SkillSchema);
